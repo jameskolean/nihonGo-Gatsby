563875cf910e577004fb2eef62fb69bbef21c4a4
James Kolean <jameskolean@yahoo.com>
2018-05-27 08:57:10 -0400

    Testing Axios

--- src/templates/CoursePageTmpl.js
+++ src/templates/CoursePageTmpl.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import LessonListing from "../components/course/lesson-listing";
+import axios from 'axios';
 
 export default class CoursePageTmpl extends Component {
-  componentDidMount() {
-    console.log('I just loaded yipee')
-  }
-  render() {
+
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    users: []
+  };
+}
+
+componentDidMount() {
+  axios.get('https://randomuser.me/api/')
+    .then(res => {
+      const users = res.data.results.map(obj => obj);
+      this.setState({ users });
+    });
+
+}
+
+render() {
     const { data } = this.props;
 //    data.markdownRemark.fields.lessons.map(node => console.log(node));
 //    console.log(data.markdownRemark.fields.lessons);
@@ -14,6 +31,16 @@ export default class CoursePageTmpl extends Component {
         <span>{data.markdownRemark.frontmatter.date}</span>
         <h1>{data.markdownRemark.frontmatter.title}</h1>
         <h4>{data.markdownRemark.frontmatter.subtitle}</h4>
+        <h4>User List</h4>
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id.value}>{user.name.first} {user.name.last}</li>
+          )}
+        </ul>
+        {data.markdownRemark.fields.lessons.map( node  => (
+          <LessonListing key={node.id} lesson={node} />
+          // <LessonListing lesson={node} />
+        ))}
         <h4>Lessons</h4>
         {data.markdownRemark.fields.lessons.map( node  => (
           <LessonListing key={node.id} lesson={node} />
